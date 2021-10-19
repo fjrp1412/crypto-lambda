9c1a3aa227122ad7bc325a45655f7077d3c7c48b
Francisco Ruiz <fruiz.rc@gmail.com>
2021-10-19 13:15:31 -0400

    ready config files

---
+++ webpack.config.js
@@ -0,0 +1,71 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+
+module.exports = {
+  entry: './src/index',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'main.js',
+    publicPath: '/',
+  },
+
+  mode: 'production',
+
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+
+  module: {
+    rules: [
+      {
+	test: /\.(js|jsx)$/,
+	exclude: /node_modules/,
+	use: {
+	  loader: 'babel-loader',
+	},
+      },
+
+      {
+	test: /\.html$/,
+	use: [
+	  {
+	    loader: 'html-loader',
+	  },
+	],
+      },
+
+      {
+	test: /\.css$/,
+	use: [
+	  {
+	    loader: MiniCssExtractPlugin.loader,
+	  },
+	  'css-loader',
+	],
+      },
+
+      {
+	test: /\.png|.jpg?e|.svg/,
+	type: 'asset/resource',
+      },
+    ],
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+      filename: './index.html',
+    }),
+    new MiniCssExtractPlugin({
+      filename: 'assets/[name].css',
+    }),
+
+  ],
+  optimization: {
+    minimize: true,
+    minimizer: [new CssMinimizerPlugin(), new TerserPlugin()],
+  },
+};
