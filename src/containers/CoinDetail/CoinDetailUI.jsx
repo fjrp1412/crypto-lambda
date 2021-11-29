b83fb25aa3ec2f59c40a216ce92de930e90597f1
Francisco Ruiz <fruiz.rc@gmail.com>
2021-11-29 18:02:28 -0400

    have to fix table sort

--- src/containers/CoinDetail/CoinDetailUI.jsx
+++ src/containers/CoinDetail/CoinDetailUI.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import numeral from 'numeral';
 import { CoinInfoUI } from './CoinInfolUI';
 import { CoinChart } from '../CoinChart';
@@ -21,6 +21,40 @@ const CoinDetailUI = ({
   setHistory,
   markets,
 }) => {
+  const [sortConfig, setSortConfig] = useState({});
+  const [sortedMarkets, setSortMarkets] = useState([]);
+
+  useEffect(() => {
+    setSortMarkets(markets.map((market, index) => ({ ...market, id: index })));
+  }, [markets]);
+
+  const sortItems = () => {
+    setSortMarkets(
+      sortedMarkets.sort((a, b) => {
+        if (a[sortConfig.key] < b[sortConfig.key]) {
+          return sortConfig.direction === 'ascending' ? -1 : 1;
+        }
+
+        if (a[sortConfig.key] > b[sortConfig.key]) {
+          return sortConfig.direction === 'descending' ? 1 : -1;
+        }
+
+        return 0;
+      })
+    );
+  };
+
+  const requestSort = key => {
+    let direction = 'ascending';
+
+    if (sortConfig.key === key && sortConfig.direction === 'ascending') {
+      direction = 'descending';
+    }
+    setSortConfig({ key, direction });
+    sortItems();
+    console.log(sortConfig);
+  };
+
   return (
     <>
       <div className="container">
@@ -48,7 +82,9 @@ const CoinDetailUI = ({
           <TableHead>
             <tr>
               <th>
-                <button type="button">Exchange</button>
+                <button type="button" onClick={() => requestSort('exchangeId')}>
+                  Exchange
+                </button>
               </th>
               <th>
                 <button type="button">Pair</button>
@@ -68,24 +104,21 @@ const CoinDetailUI = ({
             </tr>
           </TableHead>
           <TableBody>
-            {markets.map((market, index) => {
-              market.id = index;
-              return (
-                <TableItem key={market.id}>
-                  <td>{market.exchangeId}</td>
-                  <td>
-                    {market.baseSymbol}/{market.quoteSymbol}
-                  </td>
-                  <td>{numeral(market.priceUsd).format('$ 0.000a')}</td>
-                  <td>{numeral(market.volumeUsd24Hr).format('$ 0.000a')}</td>
-                  <td>
-                    {numeral(market.percentExchangeVolume / 100).format(
-                      '% 0.0000'
-                    )}
-                  </td>
-                </TableItem>
-              );
-            })}
+            {sortedMarkets.map(market => (
+              <TableItem key={market.id}>
+                <td>{market.exchangeId}</td>
+                <td>
+                  {market.baseSymbol}/{market.quoteSymbol}
+                </td>
+                <td>{numeral(market.priceUsd).format('$ 0.000a')}</td>
+                <td>{numeral(market.volumeUsd24Hr).format('$ 0.000a')}</td>
+                <td>
+                  {numeral(market.percentExchangeVolume / 100).format(
+                    '% 0.0000'
+                  )}
+                </td>
+              </TableItem>
+            ))}
           </TableBody>
         </Table>
       </div>
